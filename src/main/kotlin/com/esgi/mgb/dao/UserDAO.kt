1427fd84b19547282737a66e1f8400c2ca3bc41f
Hugo Da Roit <contact@hdaroit.fr>
2019-05-16 23:16:26 +0200

    Move dao to a proper package

--- src/main/kotlin/com/esgi/mgb/dao/UserDAO.kt
+++
@@ -1,8 +0,0 @@
-package com.esgi.mgb.dao
-
-import com.esgi.mgb.model.Bar
-import com.esgi.mgb.model.User
-import org.springframework.data.mongodb.repository.MongoRepository
-
-
-interface UserDAO : MongoRepository<User, String>
