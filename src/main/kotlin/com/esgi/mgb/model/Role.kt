12eb6d91e699426219796c9ec85f0846d9393602
nathan bessa <nathan.besssa@gmail.com>
2019-07-07 17:50:43 +0200

    feat(model): update user and role model to suit mongodb

--- src/main/kotlin/com/esgi/mgb/model/Role.kt
+++ src/main/kotlin/com/esgi/mgb/model/Role.kt
@@ -1,13 +1,11 @@
 package com.esgi.mgb.model
 
 import org.springframework.data.mongodb.core.mapping.Document
-import javax.persistence.Entity
-import javax.persistence.GeneratedValue
-import javax.persistence.GenerationType
 import javax.persistence.Id
 
 @Document(collection = "roles")
 data class Role(
-    override var rolename: String,
-    @Id override val id: String? = null
-) : AbstractRole()
+        var rolename: String,
+        @Id
+        val id: String? = null
+)
--- src/main/kotlin/com/esgi/mgb/model/User.kt
+++ src/main/kotlin/com/esgi/mgb/model/User.kt
@@ -3,21 +3,25 @@ package com.esgi.mgb.model
 import org.springframework.data.mongodb.core.mapping.Document
 import java.util.*
 import javax.persistence.*
+import java.time.LocalDate
 import javax.validation.constraints.Email
 
 @Document(collection = "users")
-data class User(
-    @Id override val id: String? = null,
-
-    override var firstname: String,
-    override var lastname: String,
-    @Email(regexp = "^[A-Za-z](.*)([@]{1})(.{1,})(\\.)(.{1,})")
-    override var email: String,
-    override var password: String,
-    override var bars: List<Bar>? = mutableListOf(),
-    override var roles: Set<Role> = HashSet(),
-    override var accountNonExpired: Boolean = true,
-    override var accountNonLocked: Boolean = true,
-    override var credentialsNonExpired: Boolean = true,
-    override var enabled: Boolean = true
-): AbstractUser()
+open class User(
+         var firstname: String,
+         var lastname: String,
+         @Email(regexp = "^[A-Za-z](.*)([@]{1})(.{1,})(\\.)(.{1,})")
+         var email: String,
+         var passWord: String,
+         var userName: String,
+         var birthDate: LocalDate,
+         @Id
+         @GeneratedValue
+         val id: String = "",
+         var listBar: MutableList<Bar>? = null,
+         var roles: Set<Role> = HashSet(),
+         var accountNonExpired: Boolean = true,
+         var accountNonLocked: Boolean = true,
+         var credentialsNonExpired: Boolean = true,
+         var enabled: Boolean = true
+)
